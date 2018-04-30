aefa1ea437fdf2c3576a3e337f5840d9d852c223
woonat01 <nwood01@unomaha.edu>
2018-04-29 20:55:32 -0500

    sync

--- django_backend/localsettings.py
+++ django_backend/localsettings.py
@@ -28,6 +28,6 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS = ['localhost', 'django', '4f8ee97e.ngrok.io']
+ALLOWED_HOSTS = ['localhost', 'django', '46a84128.ngrok.io']
 DEBUG=False
 ENVIRONMENT = "DEV"
